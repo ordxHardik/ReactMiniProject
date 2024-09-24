d326a7b6587ec98c96065ec608df5037560af748
Hardik Jain <jainhardik750@gmail.com>
2024-09-24 23:23:57 +0530

    updating forms

--- React_Form_Hooks/Form.js
+++ React_Form_Hooks/Form.js
@@ -1,13 +1,18 @@
 import React from "react"
 
 export default function Form() {
-    const [firstName, setFirstName] = React.useState("")
-    
-    console.log(firstName)
+    const [formData, setFormData] = React.useState(
+        {firstName: "", lastName: "", email: ""}
+    )
+    // by using object as above we ca use one state to render the different input
     
     function handleChange(event) {
-        // console.log(event.target.value) this is a
-        setFirstName(event.target.value)
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
     }
     
     return (
@@ -16,6 +21,24 @@ export default function Form() {
                 type="text"
                 placeholder="First Name"
                 onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+                // value is added to make form a controlable form
+                // 
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
             />
         </form>
     )
