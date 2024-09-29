fccd5390d7c044e766b0ca59e166b65da0170dab
Hardik Jain <jainhardik750@gmail.com>
2024-09-29 21:23:20 +0530

    Name Card Project Started

--- NameCard/src/App.jsx
+++ NameCard/src/App.jsx
@@ -2,7 +2,44 @@ import { useState } from 'react'
 
 import './App.css'
 
-return (
+function App(){
+
+  
+const styles = {
+  card: {
+    fontFamily: 'Arial, sans-serif',
+    border: '2px solid #007acc',
+    borderRadius: '10px',
+    padding: '20px',
+    maxWidth: '400px',
+    textAlign: 'center',
+    background: 'linear-gradient(120deg, #f0f4f8, #d9e6f2)',
+    margin: 'auto',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
+  },
+  header: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    color: '#007acc',
+    marginBottom: '10px'
+  },
+  subHeader: {
+    fontSize: '18px',
+    fontWeight: 'normal',
+    color: '#555',
+    marginBottom: '15px'
+  },
+  reactLogo: {
+    margin: '10px 0'
+  },
+  description: {
+    fontSize: '16px',
+    color: '#333',
+  }
+}
+  
+  
+  return (
   <div style={styles.card}>
     <div style={styles.header}>Hardik Jain</div>
     <div style={styles.subHeader}>Front End Developer</div>
@@ -20,37 +57,6 @@ return (
 );
 
 
-const styles = {
-card: {
-  fontFamily: 'Arial, sans-serif',
-  border: '2px solid #007acc',
-  borderRadius: '10px',
-  padding: '20px',
-  maxWidth: '400px',
-  textAlign: 'center',
-  background: 'linear-gradient(120deg, #f0f4f8, #d9e6f2)',
-  margin: 'auto',
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
-},
-header: {
-  fontSize: '24px',
-  fontWeight: 'bold',
-  color: '#007acc',
-  marginBottom: '10px'
-},
-subHeader: {
-  fontSize: '18px',
-  fontWeight: 'normal',
-  color: '#555',
-  marginBottom: '15px'
-},
-reactLogo: {
-  margin: '10px 0'
-},
-description: {
-  fontSize: '16px',
-  color: '#333',
-}
 };
 
 // export default GreetingCard;
